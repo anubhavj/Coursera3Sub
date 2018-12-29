c86d701fb288eda494f3b3f8265decb31f7bed9a
Anubhav Jain <anubhav.iim@gmail.com>
2018-12-29 11:25:10 +0530

    Roxygen2 commit

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -17,7 +17,7 @@ fars_read <- function(filename) {
   if(!file.exists(filename))
     stop("file '", filename, "' does not exist")
   data <- suppressMessages({
-    readr::read_csv(filename, progress = FALSE)
+    readr:::read_csv(filename, progress = FALSE)
   })
   dplyr::tbl_df(data)
 }
@@ -59,8 +59,8 @@ fars_read_years <- function(years) {
     file <- make_filename(year)
     tryCatch({
       dat <- fars_read(file)
-      dplyr::mutate(dat, year = year) %>%
-        dplyr::select(MONTH, year)
+      dplyr:::mutate(dat, year = year) %>%
+        dplyr:::select(MONTH, year)
     }, error = function(e) {
       warning("invalid year: ", year)
       return(NULL)
@@ -84,10 +84,10 @@ fars_read_years <- function(years) {
 
 fars_summarize_years <- function(years) {
   dat_list <- fars_read_years(years)
-  dplyr::bind_rows(dat_list) %>%
-    dplyr::group_by(year, MONTH) %>%
-    dplyr::summarize(n = n()) %>%
-    tidyr::spread(year, n)
+  dplyr:::bind_rows(dat_list) %>%
+    dplyr:::group_by(year, MONTH) %>%
+    dplyr:::summarize(n = n()) %>%
+    tidyr:::spread(year, n)
 }
 
 #' Plotting Accidents on Map for Given State
@@ -113,7 +113,7 @@ fars_map_state <- function(state.num, year) {
 
   if(!(state.num %in% unique(data$STATE)))
     stop("invalid STATE number: ", state.num)
-  data.sub <- dplyr::filter(data, STATE == state.num)
+  data.sub <- dplyr:::filter(data, STATE == state.num)
   if(nrow(data.sub) == 0L) {
     message("no accidents to plot")
     return(invisible(NULL))
